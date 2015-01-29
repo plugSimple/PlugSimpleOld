dfe559303c148f05edf24dee8b0b5e2f53dcb883
itotallyrock <itotallyrock@users.noreply.github.com>
2015-01-28 21:41:13 -0600

    Simplifying

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 plugSimple = {
 	AUTHOR: "R0CK",
-	VERSION: "v0.00.1-Beta",
+	VERSION: "v0.00.2-Beta",
 	PREFIX: "PlugSimple » ",
 	colors: {
 		ERROR: "bb0000",
@@ -17,37 +17,29 @@ plugSimple = {
 	},
 	logging: {
 		log: function(msg,debug){
-			if(debug){
-				if(plugSimple.settings.debug){
-					console.log("%c"+plugSimple.PREFIX+msg,'color: #'+plugSimple.colors.DEFAULT+'; font-weight:700;');
-				}
+			if(debug && plugSimple.settings.debug){
+				console.log("%c"+plugSimple.PREFIX+msg,'color: #'+plugSimple.colors.DEFAULT+'; font-weight:700;');
 			}else{
 				console.log("%c"+plugSimple.PREFIX+msg,'color: #'+plugSimple.colors.DEFAULT+'; font-weight:700;');
 			}
 		},
 		warn: function(msg,debug){
-			if(debug){
-				if(plugSimple.settings.debug){
-					console.warn("%c"+plugSimple.PREFIX+msg,'color: #'+plugSimple.colors.ALERT+'; font-weight:700;');
-				}
+			if(debug && plugSimple.settings.debug){
+				console.warn("%c"+plugSimple.PREFIX+msg,'color: #'+plugSimple.colors.ERROR+'; font-weight:700;');
 			}else{
-				console.warn("%c"+plugSimple.PREFIX+msg,'color: #'+plugSimple.colors.ALERT+'; font-weight:700;');
+				console.warn("%c"+plugSimple.PREFIX+msg,'color: #'+plugSimple.colors.ERROR+'; font-weight:700;');
 			}
 		},
 		error: function(msg,debug){
-			if(debug){
-				if(plugSimple.settings.debug){
-					console.error("%c"+plugSimple.PREFIX+msg,'color: #'+plugSimple.colors.WARN+'; font-weight:700;');
-				}
+			if(debug && plugSimple.settings.debug){
+				console.error("%c"+plugSimple.PREFIX+msg,'color: #'+plugSimple.colors.WARN+'; font-weight:700;');
 			}else{
 				console.error("%c"+plugSimple.PREFIX+msg,'color: #'+plugSimple.colors.WARN+'; font-weight:700;');
 			}
 		},
 		info: function(msg,debug){
-			if(debug){
-				if(plugSimple.settings.debug){
-					console.info("%c"+plugSimple.PREFIX+msg,'color: #'+plugSimple.colors.INFO+'; font-weight:700;');
-				}
+			if(debug && plugSimple.settings.debug){
+				console.info("%c"+plugSimple.PREFIX+msg,'color: #'+plugSimple.colors.INFO+'; font-weight:700;');
 			}else{
 				console.info("%c"+plugSimple.PREFIX+msg,'color: #'+plugSimple.colors.INFO+'; font-weight:700;');
 			}
@@ -101,7 +93,7 @@ plugSimple = {
 			
 			plugSimple.logging.info("Ran update in "+(new Date().getMilliseconds() - s)+"ms",true);
 		},
-		stop: function(){
+		stop: function(e){
 			plugSimple.core.saveSettings();
 			var q;
 			
@@ -111,7 +103,7 @@ plugSimple = {
 				}
 			}
 			
-			plugSimple.logging.warn("plugSimple has stopped.");
+			plugSimple.logging.warn("plugSimple has stopped "(typeof e == "undefined" ? (".")(e+".")));
 			delete plugSimple;
 		}
 	},
