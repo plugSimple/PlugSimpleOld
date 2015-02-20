9ac989553572b5d8b3c0a30380892f1b98458bf4
itotallyrock <itotallyrock@users.noreply.github.com>
2015-02-19 20:12:37 -0600

    Major Update
    Temp disable plugInterface due to bug
    Added tick method (runs every tickRate)
    Added getETA to get the eta of waitlist
    
    Moved command initialization to separate method

--- js/main.js
+++ js/main.js
@@ -16,10 +16,14 @@ plugSimple = {
 			"ac76ff" //Gaming
 		]
 	},
+	tickRate: 1,//Ticks per second
+	tickNum: 0,
+	tick: "",
 	settings: {
 		autowoot: false,
 		autodj: false,
-		debug: false
+		debug: false,
+		tickLog: false
 	},
 	logging: {
 		log: function(msg,debug){
@@ -59,7 +63,28 @@ plugSimple = {
 		getSettings: function(){
 			plugSimple.settings = JSON.parse(localStorage.getItem("plugSimple"));
 			plugSimple.logging.info("Retrieved Settings",true);
-		},	
+		},
+		getTick: function(){
+			return plugSimple.tickNum;
+		},
+		getETA: function(){
+			if(API.getDJ() != null && API.getDJ().id == API.getUser().id){
+				return "00:00:00";
+			}else{
+				var history = API.getHistory(),histlength = 0, avg = 0;
+				for(var i in history){
+					histlength += history[i].media.duration;
+				}
+				avg = histlength/history.length;
+				
+				var sn = parseInt((API.getWaitListPosition().length == -1 ? API.getWaitList().length : API.getWaitListPosition())*avg+API.getTimeRemaining(), 10);
+				var h = Math.floor(sn / 3600);
+				var m = Math.floor((sn - (h * 3600)) / 60);
+				var s = sn - (h * 3600) - (m * 60);
+
+				return (h < 10 ? "0"+h : h)+':'+(m < 10 ? "0"+m : m)+':'+(s < 10 ? "0"+s : s);
+			}
+		},
 		autoWoot: function(){
 			$("#woot").click();
 			API.on(API.ADVANCE,function(){
@@ -81,31 +106,47 @@ plugSimple = {
 		main: function(){
 			var s = new Date().getMilliseconds();
 			//LOAD EXTERNAL SCRIPTS
-			$.getScript("https://rawgit.com/itotallyrock/PlugInterfaceAPI/master/plugInterfaceAPI.js");
-			$.getScript("https://rawgit.com/itotallyrock/PlugCommandAPI/master/plugCommandAPI.js");
+			//if(typeof plugInterface == "undefined"){plugSimple.logging.log("Loaded plugInterfaceAPI status "+$.getScript("https://rawgit.com/itotallyrock/PlugInterfaceAPI/master/plugInterfaceAPI.js").readyState,true);}
+			if(typeof Command == "undefined"){plugSimple.logging.log("Loaded plugCommandAPI status "+$.getScript("https://rawgit.com/itotallyrock/PlugCommandAPI/master/plugCommandAPI.js").readyState,true);}
 			
 			if(localStorage.getItem("plugSimple") !== null){
 				plugSimple.core.getSettings();
 			}else{
 				plugSimple.core.saveSettings();
+				plugSimple.core.getSettings();
 			}
 			
 			if(plugSimple.settings.autowoot){plugSimple.core.autoWoot();}
 			if(plugSimple.settings.autodj){plugSimple.core.autoDJ();}
 			
-			var settingsCommand = new Command("settings",["type"]);
-			settingsCommand.callback = function(a){
-				a = a.toLowerCase();
-				plugSimple.settings[a[0]] = !plugSimple.settings[a[0]];
-				//plugInterface.chat("",(plugSimple.settings[a[0]] ? "Enabled" : "Disabled")+" AutoWoot"){}
-				plugSimple.logging.info((plugSimple.settings[a[0]] ? "Enabled" : "Disabled")+" "+a);
-			};
+			if(typeof Command == "Command"){plugSimple.init.cmd();}
+			
+			$("#dj-button > .bottom").css("margin-top","-40px");
 			
 			API.on(API.CHAT, function(e){
 				$(".cm[data-cid=\""+e.cid+"\"] > .badge-box").css("border","2px solid #"+plugSimple.colors.status[API.getUser(e.uid).status]);
 			});
 			
+			plugSimple.tick = setInterval(function(){plugSimple.init.tick();plugSimple.tickNum++;},plugSimple.tickRate*1000);
+			
 			plugSimple.logging.info("Started in "+(new Date().getMilliseconds() - s)+"ms");
+		},
+		tick: function(){//WILL RUN EVERY TICKRATE
+			if(plugSimple.tickNum%10 == 0 && plugSimple.settings.tickLog){
+				plugSimple.logging.log("TICK #"+plugSimple.tickNum,true);
+			}
+			$(".plugSimple-eta").text(plugSimple.core.getETA());
+		},
+		cmd: function(){//Initialize commands
+			new Command("settings",["type"]).callback = function(a){
+				a[0] = a[0].toLowerCase();
+				console.log("typeof setting = "+typeof plugSimple.settings[a[0]]);
+				if(typeof plugSimple.settings[a[0]] == "undefined"){throw new SyntaxError("Unknown Setting "+a[0]); return;}
+				plugSimple.settings[a] = !plugSimple.settings[a[0]];
+				plugInterface.chat("info",(plugSimple.settings[a[0]] ? "Enabled" : "Disabled")+" "+a[0])
+				plugSimple.logging.info((plugSimple.settings[a[0]] ? "Enabled" : "Disabled")+" "+a);
+			};
+			plugSimple.logging.log("Created settings Command",true);
 		},
 		update: function(){
 			plugSimple.core.saveSettings();
@@ -136,7 +177,7 @@ plugSimple = {
 					API.off(API[q]);
 				}
 			}
-			
+			clearInterval(plugSimple.tick);
 			plugSimple.logging.warn("plugSimple has stopped ["+e+"].");
 			delete plugSimple;
 		}
